daf5a6ad28ecd85d6fe7aec6fc037b8a1945411e
Randy King Jr <rkingjr@outlook.com>
2022-04-08 01:05:23 -0500

    add intersection failure

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
-import { update as updateSnake, draw as drawSnake, snakeSpeed } from "./snake.js"
+import { update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, snakeIntersection } from "./snake.js"
 import { update as updateFood, draw as drawFood } from "./food.js"
+import { outsideGrid } from "./grid.js "
 
 let lastRenderTime = 0
 let gameOver = false
@@ -7,7 +8,10 @@ const gameBoard = document.getElementById("game-board")
 
 function main(currentTime) {
     if (gameOver) {
-        return alert("YOU LOSE!")
+        if (confirm("You Lost. Press OK to restart.")) {
+            window.location = ''
+        }
+        return
     }
 
     window.requestAnimationFrame(main)
