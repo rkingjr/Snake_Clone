8ee9eff826c4c62ba7c35212faccb374c62991f9
Randy King Jr <rkingjr@outlook.com>
2022-04-08 00:27:14 -0500

    add grid.js with ramdon positions

--- food.js
+++ food.js
@@ -1,12 +1,13 @@
 import { onSnake, expandSnake } from "./snake.js"
+import { randomGridPosition } from "./grid.js"
 
-let food = { x: 0, y: 0 }
+let food = getRandomFoodPosition
 const expansionRate = 1
 
 export function update() {
     if (onSnake(food)) {
         expandSnake(expansionRate)
-        food = { x: 20, y: 10 }
+        food = getRandomFoodPosition()
     }
 }
 
@@ -16,4 +17,12 @@ export function draw(gameBoard) {
     foodElement.style.gridColumnStart = food.x
     foodElement.classList.add("food")
     gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
 }
